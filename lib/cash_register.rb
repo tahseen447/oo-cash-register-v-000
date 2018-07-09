e0e0cec393d162c601db334fad2f0d08b01bf2a2
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-09 21:57:02 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,6 +16,6 @@ class CashRegister
   def apply_discount
     discount = (@total * @discount)/100.0
   binding.pry
-    puts "After the discount, the total comes to $#{@total - discount }"
+    puts "After the discount, the total comes to $#{@total - discount}"
   end
 end
